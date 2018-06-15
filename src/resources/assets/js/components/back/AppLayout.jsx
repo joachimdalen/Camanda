a6351777b2c0b511f06e32feeb289fed78a754cb
Joachim Dalen <me@d3v.no>
2018-06-15 13:03:25 +0200

    Move toastr to the upper right corner

--- src/resources/assets/js/components/back/AppLayout.jsx
+++ src/resources/assets/js/components/back/AppLayout.jsx
@@ -59,7 +59,7 @@ export default class AppLayout extends Component {
                     timeOut={4000}
                     newestOnTop={false}
                     preventDuplicates
-                    position="top-left"
+                    position="top-right"
                     transitionIn="fadeIn"
                     transitionOut="fadeOut"
                     progressBar />
