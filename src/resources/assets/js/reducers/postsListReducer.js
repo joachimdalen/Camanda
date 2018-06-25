0d962ea5781143ec2cc089f2915a7c410cf54a92
Joachim Dalen <me@d3v.no>
2018-06-25 15:14:42 +0200

    Fix posts not loading

--- src/resources/assets/js/reducers/postsListReducer.js
+++ src/resources/assets/js/reducers/postsListReducer.js
@@ -1,3 +1,5 @@
+import {FETCH_POSTS, FETCH_POSTS_FULFILLED, FETCH_POSTS_REJECTED} from "../actions/postsListActions";
+
 export default function reducer(state = {
     posts: null,
     loading: false,
@@ -5,19 +7,19 @@ export default function reducer(state = {
 }, action) {
 
     switch (action.type) {
-        case "FETCH_POSTS": {
+        case FETCH_POSTS: {
             return {...state, loading: true}
-          }
-          case "FETCH_POSTS_REJECTED": {
+        }
+        case FETCH_POSTS_REJECTED: {
             return {...state, loading: false, error: action.payload}
-          }
-          case "FETCH_POSTS_FULFILLED": {
+        }
+        case FETCH_POSTS_FULFILLED: {
             return {
-              ...state,
-              loading: false,
-              posts: action.payload,
+                ...state,
+                loading: false,
+                posts: action.payload,
             }
-          }
+        }
     }
     return state
 }
