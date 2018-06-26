dbebd782b10186bf1125dceb8bd7fe2c8dc35b5a
Joachim Dalen <me@d3v.no>
2018-06-26 09:53:12 +0200

    Formatting

--- src/resources/assets/js/store/store.js
+++ src/resources/assets/js/store/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore, compose } from "redux"
 import { logger } from 'redux-logger'
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
-import { createBrowserHistory, createHashHistory } from 'history'
+import { createBrowserHistory, createHashHistory } from 'history';
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 
 import reducer from "../reducers"
@@ -14,16 +14,16 @@ import reducer from "../reducers"
 //Some rendering is handled server side, use hash router to prevent 404s.
 const history = createHashHistory({
     hashType: 'hashbang',
-})
+});
 
 //Set up middleware to handle our logic between requests.
-const middleware = applyMiddleware(promise(), thunk, logger, routerMiddleware(history))
+const middleware = applyMiddleware(promise(), thunk, logger, routerMiddleware(history));
 
 
 //Connect our router and middleware and finally create the store for Redux.
 const store = createStore(
     connectRouter(history)(reducer),
     compose(middleware),
-)
+);
 //Export store and history to be imported by the connected router and Redux provider.
 export { store, history };
